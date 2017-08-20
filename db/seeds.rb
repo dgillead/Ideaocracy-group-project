0e81120bcd360ad65d839882a71436b49ffcf56d
Jin Cheng <jincheng88@outlook.com>
2017-08-19 19:20:23 -0500

    modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@ User.all.each do |user|
   user.ideas.create!(title: Faker::Hipster.sentence, summary: Faker::Hipster.paragraphs)
 end
 
-Idea.all.each do |idea|
-  rand(2..4).times do
-    idea.comments.create!(body: Faker::Hipster.sentence)
-  end
-end
+# Idea.all.each do |idea|
+#     idea.suggestions.create!(body: Faker::Hipster.sentence)
+# end
+
+# Suggestion.all.each do |suggestion|
+#   rand(2..5).times do
+#     suggestion.comments.create!(body: Faker::Hipster.sentence)
+#   end
+# end
