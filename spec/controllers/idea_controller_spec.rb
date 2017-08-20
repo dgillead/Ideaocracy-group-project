477756c4e6975a4b431e7c37553fa5adb88dc8f8
Darnell Gillead <dlgillead@gmail.com>
2017-08-19 19:46:34 -0500

    created test and action for ideas#new rendering a form for user input

--- spec/controllers/idea_controller_spec.rb
+++ spec/controllers/idea_controller_spec.rb
@@ -3,7 +3,7 @@ require 'database_cleaner'
 
 DatabaseCleaner.strategy = :truncation
 
-RSpec.describe IdeaController, type: :controller do
+RSpec.describe IdeasController, type: :controller do
   render_views
 
   let(:user) { User.create!(first_name: 'Test first_name', last_name: 'Test last_name', email: 'test@test.com', password: 'asdfasdf') }
@@ -15,6 +15,15 @@ RSpec.describe IdeaController, type: :controller do
       get :new, params: { user_id: user.to_param }
 
       expect(assigns(:idea)).to be_a_new(Idea)
+    end
+
+    it 'renders a page for the user to create a new idea' do
+      sign_in(user)
+
+      get :new, params: { user_id: user.to_param }
+
+      expect(response.body).to include('Post an Idea')
+      expect(response.body).to include('form')
     end
   end
 
