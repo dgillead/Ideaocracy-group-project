534386dbabca3b76f63ab6d839bcccf1c26487b1
Darnell Gillead <dlgillead@gmail.com>
2017-08-27 14:43:27 -0500

    changed call in create_board method for trello api key

--- app/controllers/trello_api_controller.rb
+++ app/controllers/trello_api_controller.rb
@@ -34,7 +34,7 @@ class TrelloApiController < ApplicationController
     if params[:collaborators]
       params[:collaborators].each do |collaborator|
         user = User.find(collaborator)
-        HTTP.put("https://api.trello.com/1/boards/#{board.id}/members?email=#{user.email}&key=#{Rails.application.secrets.trello_api_key}&token=#{board.client.member_token}")
+        HTTP.put("https://api.trello.com/1/boards/#{board.id}/members?email=#{user.email}&key=#{ENV['trello_key']}&token=#{board.client.member_token}")
       end
     end
     render :success
