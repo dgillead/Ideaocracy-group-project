afc6eca8c6b78fef74a341c7b952b2fba28301d6
maliasdad09 <smithjames198955@yahoo.com>
2017-08-27 21:47:04 -0500

    added onmi to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :comments, through: :suggestions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable, 
          :recoverable, :rememberable, :trackable, :validatable
 end
